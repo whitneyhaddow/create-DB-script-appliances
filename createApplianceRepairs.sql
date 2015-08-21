3a56530d40770b047feea1d7273681ce7fc5d2ee
Whitney <whitneyhaddow@gmail.com>
2015-08-21 11:54:50 -0600

    Update createApplianceRepairs.sql
    Added customers in other states and additional info to appliances (Release Date and Years Warranty).

--- createApplianceRepairs.sql
+++ createApplianceRepairs.sql
@@ -1,14 +1,14 @@
 /****** Check to see if database already exists and if so delete ******/
-IF EXISTS(SELECT 1 FROM master.dbo.sysdatabases WHERE name = 'AppRepairs') 
+IF EXISTS(SELECT 1 FROM master.dbo.sysdatabases WHERE name = 'AppRepairsv2') 
 BEGIN
-DROP DATABASE AppRepairs
+DROP DATABASE AppRepairsv2
 END
 GO
 
-CREATE DATABASE AppRepairs
+CREATE DATABASE AppRepairsv2
 GO
 
-USE [AppRepairs]
+USE [AppRepairsv2]
 GO
 
 
@@ -23,6 +23,9 @@ GO
 CREATE TABLE [dbo].[Products](
 	[ProductCode] [char](10) NOT NULL,
 	[Name] [varchar](50) NOT NULL,
+	[YearsWarranty] [int] NOT NULL,
+	[ReleaseDate] [dateTime] NOT NULL,
+
  CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
 (
 	[ProductCode] ASC
@@ -32,15 +35,15 @@ CREATE TABLE [dbo].[Products](
 GO
 SET ANSI_PADDING OFF
 GO
-INSERT [dbo].[Products] ([ProductCode], [Name]) VALUES (N'DV5600     ', N'Electric Front-Load Dryer (Onyx)')
-INSERT [dbo].[Products] ([ProductCode], [Name]) VALUES (N'DV8700     ', N'Electric Top-Load Dryer (Stainless Platinum)')
-INSERT [dbo].[Products] ([ProductCode], [Name]) VALUES (N'DW80H9950  ', N'24 In. Dishwasher with Waterwall Technology')
-INSERT [dbo].[Products] ([ProductCode], [Name]) VALUES (N'DW80J32020 ', N'24 In. Built-In Dishwasher Stainless Steel Tub')
-INSERT [dbo].[Products] ([ProductCode], [Name]) VALUES (N'FE710DRS   ', N'Free Standing Self Clean Electric Range')
-INSERT [dbo].[Products] ([ProductCode], [Name]) VALUES (N'RF220NCTAS ', N'3-Door French Refridgerator Black')
-INSERT [dbo].[Products] ([ProductCode], [Name]) VALUES (N'RF260BEAEB ', N'French Door Refridgerator Stainless Steel')
-INSERT [dbo].[Products] ([ProductCode], [Name]) VALUES (N'WA8700     ', N'Top-Load Washer (Stainless Platinum)')
-INSERT [dbo].[Products] ([ProductCode], [Name]) VALUES (N'RWF9100    ', N'Front-Load Washer (Onyx)')
+INSERT [dbo].[Products] ([ProductCode], [Name], [YearsWarranty], [ReleaseDate]) VALUES (N'DV5600     ', N'Electric Front-Load Dryer (Onyx)', 5, CAST(0x00008EDE00000000 AS DateTime))
+INSERT [dbo].[Products] ([ProductCode], [Name], [YearsWarranty], [ReleaseDate]) VALUES (N'DV8700     ', N'Electric Top-Load Dryer (Stainless Platinum)', 5, CAST(0x00008E9200000000 AS DateTime))
+INSERT [dbo].[Products] ([ProductCode], [Name], [YearsWarranty], [ReleaseDate]) VALUES (N'DW80H9950  ', N'24 In. Dishwasher with Waterwall Technology', 3, CAST(0x00008E2F00000000 AS DateTime))
+INSERT [dbo].[Products] ([ProductCode], [Name], [YearsWarranty], [ReleaseDate]) VALUES (N'DW80J32020 ', N'24 In. Built-In Dishwasher Stainless Steel Tub', 3, CAST(0x00008EE200000000 AS DateTime))
+INSERT [dbo].[Products] ([ProductCode], [Name], [YearsWarranty], [ReleaseDate]) VALUES (N'FE710DRS   ', N'Free Standing Self Clean Electric Range', 2, CAST(0x00008EDC00000000 AS DateTime))
+INSERT [dbo].[Products] ([ProductCode], [Name], [YearsWarranty], [ReleaseDate]) VALUES (N'RF220NCTAS ', N'3-Door French Refridgerator Black', 3, CAST(0x00008E9600000000 AS DateTime))
+INSERT [dbo].[Products] ([ProductCode], [Name], [YearsWarranty], [ReleaseDate]) VALUES (N'RF260BEAEB ', N'French Door Refridgerator Stainless Steel', 3, CAST(0x00008ED000000000 AS DateTime))
+INSERT [dbo].[Products] ([ProductCode], [Name], [YearsWarranty], [ReleaseDate]) VALUES (N'WA8700     ', N'Top-Load Washer (Stainless Platinum)', 5, CAST(0x00008E9200000000 AS DateTime))
+INSERT [dbo].[Products] ([ProductCode], [Name], [YearsWarranty], [ReleaseDate]) VALUES (N'RWF9100    ', N'Front-Load Washer (Onyx)', 5, CAST(0x00008EDE00000000 AS DateTime))
 
 
 /****** Table [dbo].[Technicians] ******/
@@ -125,6 +128,18 @@ INSERT [dbo].[Customers] ([CustomerID], [Name], [City], [State], [ZipCode], [Pho
 INSERT [dbo].[Customers] ([CustomerID], [Name], [City], [State], [ZipCode], [Phone], [Email]) VALUES (1040, N'Gerald Skeitzer', N'Sacramento', N'CA', N'96351', N'(800) 550-6941', N'gskeitzer@outlook.com')
 INSERT [dbo].[Customers] ([CustomerID], [Name], [City], [State], [ZipCode], [Phone], [Email]) VALUES (1045, N'Spencer Zillow', N'Sacramento', N'CA', N'90081', N'(800) 510-1879', N'spencez50@gmail.com')
 INSERT [dbo].[Customers] ([CustomerID], [Name], [City], [State], [ZipCode], [Phone], [Email]) VALUES (1047, N'Robert Fields', N'Anaheim', N'CA', N'92807', N'(714) 892-8762', N'bobfields@gmail.com')
+INSERT [dbo].[Customers] ([CustomerID], [Name], [City], [State], [ZipCode], [Phone], [Email]) VALUES (1048, N'Tasha Williams', N'Seattle', N'WA', N'98101', N'(856) 882-1119', N'tashwilliams@gmail.com')
+INSERT [dbo].[Customers] ([CustomerID], [Name], [City], [State], [ZipCode], [Phone], [Email]) VALUES (1049, N'K''esha Teins', N'Seattle', N'WA', N'98113', N'(856) 792-5286', N'kteins89@yahoo.com')
+INSERT [dbo].[Customers] ([CustomerID], [Name], [City], [State], [ZipCode], [Phone], [Email]) VALUES (1050, N'Gregory Watkins', N'Seattle', N'WA', N'98122', N'(856) 596-8425', N'gregwatkins78@gmail.com')
+INSERT [dbo].[Customers] ([CustomerID], [Name], [City], [State], [ZipCode], [Phone], [Email]) VALUES (1051, N'Jen Benyard', N'Portland', N'OR', N'97201', N'(965) 285-1010', N'jenben@hotmail.com')
+INSERT [dbo].[Customers] ([CustomerID], [Name], [City], [State], [ZipCode], [Phone], [Email]) VALUES (1052, N'Tina Mason', N'Portland', N'OR', N'97201', N'(965) 986-1078', N'tinamason@ltereporting.com')
+INSERT [dbo].[Customers] ([CustomerID], [Name], [City], [State], [ZipCode], [Phone], [Email]) VALUES (1053, N'Jerry Wilde', N'Portland', N'OR', N'97218', N'(965) 882-7859', N'jjwilde84@hotmail.com')
+INSERT [dbo].[Customers] ([CustomerID], [Name], [City], [State], [ZipCode], [Phone], [Email]) VALUES (1054, N'C.J. Banks', N'Portland', N'OR', N'97233', N'(983) 555-7512', N'cjb@petersontowing.com')
+INSERT [dbo].[Customers] ([CustomerID], [Name], [City], [State], [ZipCode], [Phone], [Email]) VALUES (1055, N'Lois Kindleir', N'Portland', N'OR', N'97205', N'(965) 863-2253', N'loispkindleir@yahoo.com')
+INSERT [dbo].[Customers] ([CustomerID], [Name], [City], [State], [ZipCode], [Phone], [Email]) VALUES (1056, N'Rudy Thompson', N'Reno', N'NV', N'89504', N'(791) 877-7792', N'rudyt@yahoo.com')
+INSERT [dbo].[Customers] ([CustomerID], [Name], [City], [State], [ZipCode], [Phone], [Email]) VALUES (1057, N'Kent Baker', N'Reno', N'NV', N'89501', N'(791) 587-2999', N'kenty21@gmail.com')
+INSERT [dbo].[Customers] ([CustomerID], [Name], [City], [State], [ZipCode], [Phone], [Email]) VALUES (1058, N'Julie Sanchez', N'Reno', N'NV', N'89504', N'(791) 852-1788', N'jsanchez@paulaflorals.com')
+
 SET IDENTITY_INSERT [dbo].[Customers] OFF
 
 
@@ -163,7 +178,7 @@ INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) V
 INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) VALUES (1008, N'FE710DRS   ', CAST(0x00009ACD00000000 AS DateTime))
 INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) VALUES (1010, N'FE710DRS   ', CAST(0x00009BDB00000000 AS DateTime))
 INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) VALUES (1012, N'FE710DRS   ', CAST(0x00009C2D00000000 AS DateTime))
-INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) VALUES (1012, N'RF260BEAEB   ', CAST(0x00009C2D00000000 AS DateTime))
+INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) VALUES (1012, N'RF260BEAEB ', CAST(0x00009C2D00000000 AS DateTime))
 INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) VALUES (1015, N'F220NCTAS  ', CAST(0x00009AFD00000000 AS DateTime))
 INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) VALUES (1016, N'F220NCTAS  ', CAST(0x00009E2E00000000 AS DateTime))
 INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) VALUES (1017, N'F220NCTAS  ', CAST(0x00009C6000000000 AS DateTime))
@@ -187,7 +202,10 @@ INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) V
 INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) VALUES (1045, N'RF260BEAEB ', CAST(0x00009D5800000000 AS DateTime))
 INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) VALUES (1047, N'DW80H9950  ', CAST(0x00009D7600000000 AS DateTime))
 INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) VALUES (1047, N'DV5600     ', CAST(0x00009E3B00000000 AS DateTime))
-INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) VALUES (1047, N'RWF9100    ', CAST(0x00009E3B00000000 AS DateTime))
+INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) VALUES (1047, N'RWF9100    ', CAST(0x00009DA000000000 AS DateTime))
+INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) VALUES (1058, N'FE710DRS   ', CAST(0x00009DA800000000 AS DateTime))
+INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) VALUES (1050, N'WA8700     ', CAST(0x00009DBA00000000 AS DateTime))
+INSERT [dbo].[Registrations] ([CustomerID], [ProductCode], [RegistrationDate]) VALUES (1053, N'RWF9100    ', CAST(0x00009DCC00000000 AS DateTime))
 
 
 /****** Table [dbo].[Repairs] ******/
@@ -218,24 +236,26 @@ GO
 SET ANSI_PADDING OFF
 GO
 SET IDENTITY_INSERT [dbo].[Repairs] ON
-INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (1,  1010, N'FE710DRS    ', 284, CAST(0x00009E2500000000 AS DateTime), CAST(0x00009E2600000000 AS DateTime), N'Will not turn on', N'Randomly stopped working.')
-INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (2,  1017, N'F220NCTAS   ', 284, CAST(0x00009E3400000000 AS DateTime), NULL, N'Freezer light not working', N'Works intermittently with some flickering, possible faulty wiring.')
-INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (3,  1016, N'F220NCTAS   ', 296, CAST(0x00009E3400000000 AS DateTime), NULL, N'Third tray needs replacement', N'Cracked through center.')
-INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (4,  1008, N'DV5600      ', 288, CAST(0x00009E3500000000 AS DateTime), CAST(0x00009E3800000000 AS DateTime), N'Unit needs replacing', N'Caught on fire with normal use, please replace entire unit.')
-INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (5,  1047, N'RWF9100     ', 289, CAST(0x00009FC100000000 AS DateTime), NULL, N'Water leaking', N'Small amounts of water leak through the door while running.')
-INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (6,  1004, N'WA8700      ', 301, CAST(0x00009E5600000000 AS DateTime), NULL, N'Agitator needs replacing', N'Agitator quit working, not sure how long it''s been out of operation.')
-INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (7,  1012, N'RF260BEAEB  ', 284, CAST(0x00009E5900000000 AS DateTime), CAST(0x00009E5B00000000 AS DateTime), N'Freezer not getting cold enough', N'Ice/foods will not stay frozen.')
-INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (8,  1015, N'F220NCTAS   ', 288, CAST(0x00009E5A00000000 AS DateTime), NULL, N'Right inside drawer not working smoothly', N'Drawer has been getting difficult to pull out or push in.')
-INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (9,  1038, N'DW80J32020  ', 288, CAST(0x00009E6500000000 AS DateTime), NULL, N'Venting error', N'Improper venting causing adjacent cabinet to crack.')
-INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (10, 1047, N'RWF9100     ', 301, CAST(0x00009E6600000000 AS DateTime), NULL, N'Water will not get hot', N'Hot cycles not getting very hot.')
-INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (11, 1018, N'DW80J32020  ', 289, CAST(0x00009E6600000000 AS DateTime), NULL, N'Dishwasher too loud', N'Customer believes the dishwasher is making more noise than it should.')
+INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (1,  1010, N'FE710DRS    ', 284,  CAST(0x00009E2500000000 AS DateTime), CAST(0x00009E2600000000 AS DateTime), N'Will not turn on', N'Randomly stopped working.')
+INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (2,  1017, N'F220NCTAS   ', 284,  CAST(0x00009E3400000000 AS DateTime), NULL, N'Freezer light not working', N'Works intermittently with some flickering, possible faulty wiring.')
+INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (3,  1016, N'F220NCTAS   ', 296,  CAST(0x00009E3400000000 AS DateTime), NULL, N'Third tray needs replacement', N'Cracked through center.')
+INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (4,  1008, N'DV5600      ', 288,  CAST(0x00009E3500000000 AS DateTime), CAST(0x00009E3800000000 AS DateTime), N'Unit needs replacing', N'Caught on fire with normal use, please replace entire unit.')
+INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (5,  1047, N'RWF9100     ', 289,  CAST(0x00009FC100000000 AS DateTime), NULL, N'Water leaking', N'Small amounts of water leak through the door while running.')
+INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (6,  1004, N'WA8700      ', 301,  CAST(0x00009E5600000000 AS DateTime), CAST(0x00009E5A00000000 AS DateTime), N'Agitator needs replacing', N'Agitator quit working, not sure how long it''s been out of operation.')
+INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (7,  1012, N'RF260BEAEB  ', 284,  CAST(0x00009E5900000000 AS DateTime), CAST(0x00009E5B00000000 AS DateTime), N'Freezer not getting cold enough', N'Ice/foods will not stay frozen.')
+INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (8,  1015, N'F220NCTAS   ', 288,  CAST(0x00009E5A00000000 AS DateTime), NULL, N'Right inside drawer not working smoothly', N'Drawer has been getting difficult to pull out or push in.')
+INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (9,  1038, N'DW80J32020  ', 288,  CAST(0x00009E6500000000 AS DateTime), NULL, N'Venting error', N'Improper venting causing adjacent cabinet to crack.')
+INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (10, 1047, N'RWF9100     ', 301,  CAST(0x00009E6600000000 AS DateTime), NULL, N'Water will not get hot', N'Hot cycles not getting very hot.')
+INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (11, 1018, N'DW80J32020  ', 289,  CAST(0x00009E6600000000 AS DateTime), NULL, N'Dishwasher too loud', N'Customer believes the dishwasher is making more noise than it should.')
 INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (12, 1016, N'F220NCTAS   ', NULL, CAST(0x00009E7000000000 AS DateTime), NULL, N'Fridge stopped working', N'Does not get cold and lights won''t turn on.')
 INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (13, 1034, N'RWF9100     ', NULL, CAST(0x00009E7800000000 AS DateTime), NULL, N'Leaking water', N'Water leaks out the front while running.')
 INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (14, 1045, N'RF260BEAEB  ', NULL, CAST(0x00009E8700000000 AS DateTime), NULL, N'Operates loudly', N'Customer complains that fridge is running far too loud.')
 INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (15, 1012, N'FE710DRS    ', NULL, CAST(0x00009E8A00000000 AS DateTime), NULL, N'Top left burner not working', N'All other burners appear to perform normally.')
 INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (16, 1010, N'FE710DRS    ', NULL, CAST(0x00009E9A00000000 AS DateTime), NULL, N'Interior glass cracked', N'Interior glass pane has a large crack spanning the entire width of viewing window. ')
 INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (17, 1029, N'DV8700      ', NULL, CAST(0x00009E9C00000000 AS DateTime), NULL, N'Clunking noise will running.', N'Customer has noticed a cluncking noise with the past several uses.')
-INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (18, 1002, N'DV5600      ', NULL, CAST(0x00009E9D00000000 AS DateTime), NULL, N'Clothing routinely not drying', N'Customer states that the dryer worked fine previously and only recently has been performing badly.')
+INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (18, 1058, N'FE710DRS    ', NULL, CAST(0x00009EA200000000 AS DateTime), NULL, N'2 burners failed.', N'Customer reports both left side burners do no work.')
+INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (19, 1053, N'RWF9100     ', NULL, CAST(0x00009EA500000000 AS DateTime), NULL, N'Will not turn on.', N'Possible overheating - washer was run on hot cycle several times consecutively prior to failing.')
+INSERT [dbo].[Repairs] ([RepairID], [CustomerID], [ProductCode], [TechID], [DateReported], [DateRepaired], [Title], [Description]) VALUES (20, 1050, N'WA8700      ', NULL, CAST(0x00009EE000000000 AS DateTime), NULL, N'Mouldy smell.', N'Customer has reported washer smelling mouldy despite only owning it for 3 months.')
 SET IDENTITY_INSERT [dbo].[Repairs] OFF
 
 
